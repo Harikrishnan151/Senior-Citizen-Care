465e4c1b70d671f8f50bd33c1a02922dfd95443a
Your Name <harikrishnan.v.v1234@gmail.com>
2024-03-04 16:32:50 +0530

    serviceRegisteration

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,9 +4,16 @@ const express=require('express')
 //2) import controllers
 const userController=require('../controller/userController')
 const adminController=require('../controller/adminController')
+const serviveproviderController=require('../controller/serviceproviderController')
 
-// //4) import verifyUser.js file
-// const {verifyUser} =require('../utlis/verifyUser')
+//import certificate multer file
+const upload=require('../multer/storageConfig')
+
+//import image multer file
+const uploadImg=require('../multer/storageConfigImg')
+const uploadPDF = require('../multer/storageConfig')
+
+
 
 //3) Using express create a object for router class in order to setpath
 const router=new express.Router()
@@ -31,9 +38,16 @@ router.delete('/deleteUser/:id', userController.deleteUser);
 //Edit user API call
 router.post('/editUser/:id',userController.editUser)
 
-//Admin Login
+//Admin Login API call
 router.post('/adminLogin',adminController.adminLogin)
-//
+
+//serviceProvider primary registeration API call
+router.post('/serviceProvier/register',uploadPDF.single('experience_crt'),serviveproviderController.serviceProviderRegisteration)
+
+//serviceProvider secondary registration API call
+router.post('/serviceProvider/fianlRegtration',uploadImg.single('profile_img'),serviveproviderController.finalReg)
+
+
 //4) export routes
 module.exports=router
 
