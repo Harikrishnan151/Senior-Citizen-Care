cf9e1a0fd3f5a00e68addc43f80e037ea3db6ae0
AryamolMs <aryamolsimi@gmail.com>
2024-04-04 15:02:30 +0530

    Hooi

--- server/multer/storageConfig.js
+++ server/multer/storageConfig.js
@@ -24,7 +24,7 @@ const pdffileFilter=(req,file,callback)=>{
     }
 }
 
-//define upload
+//define upload-*
 const uploadPDF = multer({storage:pdfstorage,fileFilter:pdffileFilter})
 
 module.exports= uploadPDF
